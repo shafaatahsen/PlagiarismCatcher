a41d88f66ea1b0fa4efdb4e91c5648f528e101b5
felipe1997palacios <31520833+felipe1997palacios@users.noreply.github.com>
2018-12-02 16:23:43 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <iostream>
 #include "plagiarismCatcher.h"
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -19,5 +21,18 @@ int main()
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i << files[i] << endl;
     }
+
+    FILE *fptr;
+    vector <char>  eqn;
+    strcpy(eqn, files[2]);
+    fptr = fopen(eqn, "r");
+
+    if (fptr == NULL) {  //if nothing in file deosn't work
+        printf("Nothing has opened!\n");
+        exit(-1);
+    } else {        //if file has pointer in it then it works
+        printf("file open\n");
+    }
+
     return 0;
 }
