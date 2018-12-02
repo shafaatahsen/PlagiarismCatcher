a65820828cc498b9a23d4249345f957eb3b7c575
shafaat <12536813+zanelias@users.noreply.github.com>
2018-12-02 16:22:44 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "plagiarismCatcher.h"
 
 using namespace std;
@@ -12,12 +14,26 @@ using namespace std;
 int main()
 {
     string dir = string("sm_doc_set");
+    string line;
     vector<string> files = vector<string>();
 
     getdir(dir,files);
 
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i << files[i] << endl;
+        string fPath = dir + "/" + files[i];
+
+        ifstream myFile (fPath.c_str());
+
+        if (myFile.is_open())
+        {
+            while ( getline (myFile,line) )
+            {
+                cout << line << '\n';
+            }
+            myFile.close();
+        }
+
     }
     return 0;
 }
