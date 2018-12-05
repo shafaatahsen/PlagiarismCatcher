f33cec43f2407162215018c94debb4a590994041
felipe1997palacios <31520833+felipe1997palacios@users.noreply.github.com>
2018-12-04 19:03:25 -0600

    fixed hash stuff

--- hashMap.cpp
+++ hashMap.cpp
@@ -1,9 +1,9 @@
 #include "hashMap.h"
 #include <cmath>
 
-int HashMap::hashFunc(vector<char> k) {
+unsigned long HashMap::hashFunc(deque<string> k) {
 
-    int hash = 0;
+    unsigned long hash = 0;
     for(int i = 0; i < k.size(); i++){  //uses method 3 from slides
         if((k[i] > 0x40 && k[i] <0x5B) && k[i] < 0x7B) {    //only lets letters through
             if(k[i] > 0x40 && k[i] < 0x61) {
